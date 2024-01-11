5bbc6534dec60e0a48cacbba5ea83dfe895ee598
wrwrq <154485017+wrwrq@users.noreply.github.com>
2024-01-11 09:19:24 +0900

    Initial commit

---
+++ 626/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _626
+{
+    public class Solution
+    {
+        public int solution(int num1, int num2)
+        {
+            int answer = 0;
+            return answer;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
