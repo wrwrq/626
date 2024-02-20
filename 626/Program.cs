8b0b1e20c09de655fcdcc17d3789f3898e8d36a4
wrwrq <154485017+wrwrq@users.noreply.github.com>
2024-02-20 09:57:45 +0900

    a

--- 626/Program.cs
+++ 626/Program.cs
@@ -641,27 +641,26 @@ public class Solutionkata
         }
         return strings;
     }
-}
-
-internal class Program
-{
-    public static int[] solution(int[] array, int[,] commands)
+    public static int[] solution48(int[] array, int[,] commands)
     {
         int[] temp = new int[commands.GetLength(0)];
         for (int i = 0; i < commands.GetLength(0); i++)
         {
             List<int> t = new List<int>();
-            for (int p = commands[i,0] - 1; p < commands[i,1]; p++)
+            for (int p = commands[i, 0] - 1; p < commands[i, 1]; p++)
             {
                 t.Add(array[p]);
                 t.Sort();
             }
-            temp[i] = t[commands[i, 2]];
+            temp[i] = t[commands[i, 2] - 1];
         }
         return temp;
     }
+}
+
+internal class Program
+{
     static void Main(string[] args)
     {
-        Console.WriteLine(solution(new int[] { 1, 5, 2, 6, 3, 7, 4 }, new int[,] { { 2, 5, 3 }, { 4, 4, 1 }, { 1, 7, 3 } }));
     }
 }
